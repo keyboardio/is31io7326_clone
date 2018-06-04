4e465d4fcd8c39594ffb365d9d110272f8be43a9
Gravemind <gravemind2a@gmail.com>
2018-06-04 11:05:26 +0200

    led-spiout better perf: SPI freq back to 512kHz
    Real performance gain: `chatter-defense` eats less keystrokes while
    rainbow wave is running.
    
    And it keeps `led_init`'s 'all led off' at 1 MHz to keep fast 'all led off'.
    
    'all led off' is now done synchronously without interrupt to avoid
    interference with the added second `led_spi_frequency(DEFAULT)`.
    
    Which should also make it a bit faster so reducing the time leds are
    flashing/lit from power up to this.

--- firmware/wire-protocol-constants.h
+++ firmware/wire-protocol-constants.h
@@ -22,7 +22,10 @@
 
 // 512KHZ seems to be the sweet spot in early testing
 // so make it the default
-#define LED_SPI_FREQUENCY_DEFAULT LED_SPI_FREQUENCY_1MHZ
+#define LED_SPI_FREQUENCY_DEFAULT LED_SPI_FREQUENCY_512KHZ
+
+// Only for fast led_init, then set to LED_SPI_FREQUENCY_DEFAULT
+#define LED_SPI_FREQUENCY_AT_INIT LED_SPI_FREQUENCY_1MHZ
 
 
 #define TWI_CMD_LED_UPDATE_ALL 0x79
